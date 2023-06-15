c1272f44f611b54bf2e062217ecda2a3373df4c0
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-06-15 22:37:24 +0200

    styles update

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   const [goalText, setGoalText] = useState("");
@@ -8,19 +15,29 @@ export default function App() {
 
   const InputHandler = (enteredText) => {
     setGoalText(enteredText);
-  }
+  };
 
   const addGoal = () => {
     setGoalsLIst((currentList) => [...currentList, goalText]);
   };
   return (
     <View style={styles.container}>
-      <View>
-      <TextInput placeholder="Enter your goal" onChangeText={InputHandler}/>
-      <Button title="Add Goal" onPress={addGoal}/>
+      <View style={styles.inputBox}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your goal"
+          onChangeText={InputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoal} />
       </View>
-      <View>
-        {goalsList.map((goal) => <Text key={goal}>{goal}</Text>)}
+      <View style={styles.goalsBox}>
+        <ScrollView alwaysBounceVertical={false}>
+          {goalsList.map((goal) => (
+            <View style={styles.goalItem} key={goal}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -30,8 +47,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFEAD0",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputBox: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#96616B",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#96616B",
+    borderRadius: 6,
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsBox: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#113537",
+  },
+  goalText: {
+    color: "#fff",
   },
 });
