879aa27b430ecdcb4d95a5d19827daa6d444f328
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-06-29 23:00:07 +0200

    style update

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,22 +1,30 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 
-export const GolaItem = ({text, onDeleteItem, id}) => {
+export const GolaItem = ({ text, onDeleteItem, id }) => {
   return (
-    <Pressable onPress={onDeleteItem.bind(this, id)}>
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{text}</Text>
-    </View></Pressable>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={onDeleteItem.bind(this, id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{text}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#113537",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: "#fff",
   },
 });
